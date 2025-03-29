52bf3c22ea17d755cfd95987142952ef204498a0
b22thohe <tomhell66@gmail.com>
2025-03-30 00:29:16 +0100

    Misc test code

--- src/jweather/Main.java
+++ src/jweather/Main.java
@@ -3,5 +3,11 @@ package jweather;
 public class Main {
     public static void main(String[] args) {
         MainView gui = new MainView("JWeather 1.0");
+        Model model = new Model();
+        Controller controller = new Controller(gui, model);
+
+        // Test run - output console
+        controller.getWeatherData();
+        controller.displayWeatherData();
     }
 }
