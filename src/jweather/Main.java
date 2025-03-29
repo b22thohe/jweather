ec54536b19c9be520a7f21a7b1a0516775549b8d
b22thohe <tomhell66@gmail.com>
2025-03-29 21:37:58 +0100

    Add Main (start) class and main gui class

---
+++ src/jweather/Main.java
@@ -0,0 +1,7 @@
+package jweather;
+
+public class Main {
+    public static void main(String[] args) {
+        MainView gui = new MainView("JWeather 1.0");
+    }
+}
---
+++ src/jweather/MainView.java
@@ -0,0 +1,26 @@
+package jweather;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainView extends JFrame {
+
+    // Constructor
+    public MainView(String title) {
+        super(title);
+
+        // Enable window buttons functionality
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Set size and preferred size of application window
+        this.setSize(600, 400);
+        this.setPreferredSize(new Dimension(600, 400));
+        // Set layout manager of frame
+        this.setLayout(new BorderLayout());
+
+        // Display the window
+        this.setVisible(true);
+
+        revalidate();
+        repaint();
+    }
+}
