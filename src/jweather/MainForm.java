0e82278d76f836b08f5729a3b5ed74a82c123396
b22thohe <tomhell66@gmail.com>
2025-07-20 15:10:30 +0200

    New layout

---
+++ src/jweather/MainForm.java
@@ -0,0 +1,24 @@
+package jweather;
+
+import javax.swing.*;
+
+public class MainForm {
+    private JPanel mainPanel;
+    private JPanel topPanel;
+    private JTextField searchInput;
+    private JButton searchButton;
+    private JPanel bottomPanel;
+    private JLabel searchLabel;
+    private JLabel tempLabel;
+    private JLabel feelsLikeLabel;
+    private JLabel tempOutput;
+    private JLabel feelsLikeOutput;
+    private JLabel minTempLabel;
+    private JLabel maxTempLabel;
+    private JLabel minTempOutput;
+    private JLabel maxTempOutput;
+    private JLabel windLabel;
+    private JLabel humidityLabel;
+    private JLabel windOutput;
+    private JLabel humidityOutput;
+}
